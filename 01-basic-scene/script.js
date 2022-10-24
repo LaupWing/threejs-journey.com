48b8add73922bf7ab49ed19e18bfb4a73900fa91
LaupWing <loc_strict@hotmail.com>
2022-10-24 15:31:29 +0200

    udpate

--- 01-basic-scene/script.js
+++ 01-basic-scene/script.js
@@ -1,3 +1,23 @@
-import * as THREE from 'three';
+import * as THREE from "three"
 
-console.log(THREE)
+const canvas = document.querySelector(".webgl")
+const scene = new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({
+   color: "red"
+})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+const sizes = {
+   width: 800,
+   height: 600
+}
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+scene.add(camera)
+
+const renderer = new THREE.WebGLRenderer({
+   canvas
+})
