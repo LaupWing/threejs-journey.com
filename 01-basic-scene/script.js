c637b9eb52baa83123bb7efe2dfd34bb1718d2ef
LaupWing <loc_strict@hotmail.com>
2022-10-24 15:37:46 +0200

    basic scene

--- 01-basic-scene/script.js
+++ 01-basic-scene/script.js
@@ -16,8 +16,11 @@ const sizes = {
 }
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 5
 scene.add(camera)
 
 const renderer = new THREE.WebGLRenderer({
    canvas
 })
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
