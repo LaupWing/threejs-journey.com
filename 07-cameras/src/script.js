56360d275ded6349adfb59a7d0fc8c05234c75d3
LaupWing <loc_strict@hotmail.com>
2022-10-26 06:00:19 +0200

    udpate

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -1,6 +1,16 @@
 import './style.css'
 import * as THREE from 'three'
 
+const cursor = {
+   x: 0,
+   y: 0
+}
+
+window.addEventListener("mousemove", (event) => {
+   cursor.x = event.clientX / sizes.width - 0.5
+   cursor.y = -(event.clientY / sizes.height - 0.5)
+})
+
 /**
  * Base
  */
@@ -24,10 +34,19 @@ const mesh = new THREE.Mesh(
 scene.add(mesh)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
+// const aspectRatio = sizes.width / sizes.height
+// const camera = new THREE.OrthographicCamera(
+//    -1 * aspectRatio, 
+//    1 * aspectRatio, 
+//    1, 
+//    -1, 
+//    0.1, 
+//    100
+// )
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 4
 camera.lookAt(mesh.position)
 scene.add(camera)
 
@@ -43,8 +62,12 @@ const clock = new THREE.Clock()
 const tick = () => {
    const elapsedTime = clock.getElapsedTime()
 
-   // Update objects
-   mesh.rotation.y = elapsedTime;
+   // // Update objects
+   // mesh.rotation.y = elapsedTime;
+   // mesh.rotation.y = elapsedTime;
+   camera.position.x = Math.sin(cursor.x * 3) * 3
+   camera.position.z = Math.cos(cursor.x * 3) * 3
+   camera.lookAt(mesh.position)
 
    // Render
    renderer.render(scene, camera)
