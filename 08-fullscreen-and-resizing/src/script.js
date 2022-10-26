481e00311d89680e5fb292112efd35b1959d248a
LaupWing <loc_strict@hotmail.com>
2022-10-26 15:37:33 +0200

    upfsryyyyyyyy

--- 08-fullscreen-and-resizing/src/script.js
+++ 08-fullscreen-and-resizing/src/script.js
@@ -23,9 +23,28 @@ scene.add(mesh)
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
+   width: window.innerWidth,
+   height: window.innerHeight
 }
+
+window.addEventListener("resize", ()=>{
+   sizes.width = window.innerWidth
+   sizes.height = window.innerHeight
+
+   camera.aspect = sizes.width / sizes.height
+   camera.updateProjectionMatrix()
+
+   renderer.setSize(sizes.width, sizes.height)
+   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+window.addEventListener("dblclick", ()=>{
+   if(!document.fullscreenElement){
+      canvas.requestFullscreen()
+   }else{
+      document.exitFullscreen()
+   }
+})
 
 /**
  * Camera
@@ -43,27 +62,27 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+   canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animate
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+   const elapsedTime = clock.getElapsedTime()
 
-    // Update controls
-    controls.update()
+   // Update controls
+   controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+   // Render
+   renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+   // Call tick again on the next frame
+   window.requestAnimationFrame(tick)
 }
 
 tick()
