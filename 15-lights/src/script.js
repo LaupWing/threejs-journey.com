15e3e4cd5081245bcfee30a6a17e8c4b9f731efd
LaupWing <loc_strict@hotmail.com>
2022-10-31 16:44:16 +0100

    update

--- 15-lights/src/script.js
+++ 15-lights/src/script.js
@@ -21,11 +21,34 @@ const scene = new THREE.Scene()
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
 scene.add(ambientLight)
 
-const pointLight = new THREE.PointLight(0xffffff, 0.5)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
+const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3)
+
+directionalLight.position.set(1, 0.25, 0)
+scene.add(directionalLight)
+
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3)
+scene.add(hemisphereLight)
+
+const pointLight = new THREE.PointLight(0xff9000, 0.5, 10, 2)
+pointLight.position.set(1, -0.5, 1)
 scene.add(pointLight)
+
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 1, 1)
+rectAreaLight.position.set(-1.5, 0, 1.5)
+rectAreaLight.lookAt(new THREE.Vector3())
+scene.add(rectAreaLight)
+
+const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 10, Math.PI * 0.1, 0.25, 1)
+spotLight.position.set(0, 2, 3)
+scene.add(spotLight)
+
+spotLight.target.position.x = 0.75
+scene.add(spotLight.target)
+
+const hemispehereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2)
+scene.add(hemispehereLightHelper)
+
+// const ambientLight =
 
 /**
  * Objects
@@ -36,25 +59,25 @@ material.roughness = 0.4
 
 // Objects
 const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 32, 32),
-    material
+   new THREE.SphereGeometry(0.5, 32, 32),
+   material
 )
 sphere.position.x = - 1.5
 
 const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(0.75, 0.75, 0.75),
-    material
+   new THREE.BoxGeometry(0.75, 0.75, 0.75),
+   material
 )
 
 const torus = new THREE.Mesh(
-    new THREE.TorusGeometry(0.3, 0.2, 32, 64),
-    material
+   new THREE.TorusGeometry(0.3, 0.2, 32, 64),
+   material
 )
 torus.position.x = 1.5
 
 const plane = new THREE.Mesh(
-    new THREE.PlaneGeometry(5, 5),
-    material
+   new THREE.PlaneGeometry(5, 5),
+   material
 )
 plane.rotation.x = - Math.PI * 0.5
 plane.position.y = - 0.65
@@ -65,23 +88,22 @@ scene.add(sphere, cube, torus, plane)
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+   width: window.innerWidth,
+   height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+   // Update sizes
+   sizes.width = window.innerWidth
+   sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+   // Update camera
+   camera.aspect = sizes.width / sizes.height
+   camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+   // Update renderer
+   renderer.setSize(sizes.width, sizes.height)
+   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -102,7 +124,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+   canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -112,27 +134,26 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+   const elapsedTime = clock.getElapsedTime()
 
-    // Update objects
-    sphere.rotation.y = 0.1 * elapsedTime
-    cube.rotation.y = 0.1 * elapsedTime
-    torus.rotation.y = 0.1 * elapsedTime
+   // Update objects
+   sphere.rotation.y = 0.1 * elapsedTime
+   cube.rotation.y = 0.1 * elapsedTime
+   torus.rotation.y = 0.1 * elapsedTime
 
-    sphere.rotation.x = 0.15 * elapsedTime
-    cube.rotation.x = 0.15 * elapsedTime
-    torus.rotation.x = 0.15 * elapsedTime
+   sphere.rotation.x = 0.15 * elapsedTime
+   cube.rotation.x = 0.15 * elapsedTime
+   torus.rotation.x = 0.15 * elapsedTime
 
-    // Update controls
-    controls.update()
+   // Update controls
+   controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+   // Render
+   renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+   // Call tick again on the next frame
+   window.requestAnimationFrame(tick)
 }
 
 tick()
