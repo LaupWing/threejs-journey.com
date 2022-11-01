5def1d0ee026d67ed3d3754cd2b5aab1dbe2b522
LaupWing <loc_strict@hotmail.com>
2022-11-01 16:41:17 +0100

    update

--- 17-haunted-house/src/script.js
+++ 17-haunted-house/src/script.js
@@ -23,18 +23,59 @@ const textureLoader = new THREE.TextureLoader()
 /**
  * House
  */
-// Temporary sphere
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(1, 32, 32),
-    new THREE.MeshStandardMaterial({ roughness: 0.7 })
+const house = new THREE.Group()
+
+// Walls
+const walls = new THREE.Mesh(
+   new THREE.BoxGeometry(4, 2.5, 4),
+   new THREE.MeshStandardMaterial({ color: "#ac8e82" })
 )
-sphere.position.y = 1
-scene.add(sphere)
+walls.position.y = 1.25
+house.add(walls)
+
+const roof = new THREE.Mesh(
+   new THREE.ConeGeometry(3.5, 1, 4),
+   new THREE.MeshBasicMaterial({ color: "#b35f45" })
+)
+roof.rotation.y = Math.PI * 0.25
+roof.position.y = 2.5 + 0.5
+house.add(roof)
+
+const door = new THREE.Mesh(
+   new THREE.PlaneGeometry(2, 2),
+   new THREE.MeshStandardMaterial({ color: "#aa7b7b" })
+)
+door.position.y = 1
+door.position.z = 2 + 0.01
+house.add(door)
+
+// Bushes
+const bushGeometry = new THREE.SphereGeometry(1, 16, 16)
+const bushMaterial = new THREE.MeshStandardMaterial({ color: '#89c854' })
+
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush1.scale.set(0.5, 0.5, 0.5)
+bush1.position.set(0.8, 0.2, 2.2)
+
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush2.scale.set(0.25, 0.25, 0.25)
+bush2.position.set(1.4, 0.1, 2.1)
+
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush3.scale.set(0.4, 0.4, 0.4)
+bush3.position.set(- 0.8, 0.1, 2.2)
+
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush4.scale.set(0.15, 0.15, 0.15)
+bush4.position.set(- 1, 0.05, 2.6)
+
+house.add(bush1, bush2, bush3, bush4)
+scene.add(house)
 
 // Floor
 const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(20, 20),
-    new THREE.MeshStandardMaterial({ color: '#a9c388' })
+   new THREE.PlaneGeometry(20, 20),
+   new THREE.MeshStandardMaterial({ color: '#a9c388' })
 )
 floor.rotation.x = - Math.PI * 0.5
 floor.position.y = 0
@@ -61,23 +102,22 @@ scene.add(moonLight)
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+   width: window.innerWidth,
+   height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+   // Update sizes
+   sizes.width = window.innerWidth
+   sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+   // Update camera
+   camera.aspect = sizes.width / sizes.height
+   camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+   // Update renderer
+   renderer.setSize(sizes.width, sizes.height)
+   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -98,7 +138,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+   canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -108,18 +148,17 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+   const elapsedTime = clock.getElapsedTime()
 
-    // Update controls
-    controls.update()
+   // Update controls
+   controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+   // Render
+   renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+   // Call tick again on the next frame
+   window.requestAnimationFrame(tick)
 }
 
 tick()
