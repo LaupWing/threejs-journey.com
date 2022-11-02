41817928b074ba4cde09f6f3fbe299a803739f29
LaupWing <loc_strict@hotmail.com>
2022-11-02 16:44:07 +0100

    update

--- 18-particles/src/script.js
+++ 18-particles/src/script.js
@@ -20,36 +20,46 @@ const scene = new THREE.Scene()
  */
 const textureLoader = new THREE.TextureLoader()
 
-/**
- * Test cube
- */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+const particlesGeometry = new THREE.BufferGeometry()
+
+const count = 500000
+
+const positions = new Float32Array(count * 3)
+
+for(let i = 0; i< count; i++){
+   positions[i] = (Math.random() - 0.5) * 10
+}
+
+particlesGeometry.setAttribute("position", new THREE.BufferAttribute(positions, 3))
+
+const particlesMaterial = new THREE.PointsMaterial({
+   size: 0.02,
+   sizeAttenuation: true
+})
+
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+   width: window.innerWidth,
+   height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+   // Update sizes
+   sizes.width = window.innerWidth
+   sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+   // Update camera
+   camera.aspect = sizes.width / sizes.height
+   camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+   // Update renderer
+   renderer.setSize(sizes.width, sizes.height)
+   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -68,7 +78,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+   canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -78,18 +88,17 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+   const elapsedTime = clock.getElapsedTime()
 
-    // Update controls
-    controls.update()
+   // Update controls
+   controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+   // Render
+   renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+   // Call tick again on the next frame
+   window.requestAnimationFrame(tick)
 }
 
 tick()
