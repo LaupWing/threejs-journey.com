9728bd02ad8aa1c4e4434ac5143f18e21a89d95f
LaupWing <loc_strict@hotmail.com>
2022-11-07 05:54:13 +0100

    gradient

--- 21-scroll-based-animation/src/script.js
+++ 21-scroll-based-animation/src/script.js
@@ -28,6 +28,7 @@ const scene = new THREE.Scene()
 
 const textureLoader = new THREE.TextureLoader()
 const gradientTexture = textureLoader.load("textures/gradient/3.jpg")
+gradientTexture.magFilter = THREE.NearestFilter
 
 
 const material = new THREE.MeshToonMaterial({
