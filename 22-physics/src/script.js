88b68dc4cacad50b75e0fc90db363d6dc4ad2b40
LaupWing <loc_strict@hotmail.com>
2022-11-08 06:20:58 +0100

    bounce

--- 22-physics/src/script.js
+++ 22-physics/src/script.js
@@ -37,11 +37,25 @@ const environmentMapTexture = cubeTextureLoader.load([
 const world = new CANNON.World()
 world.gravity.set(0, -9.82, 0)
 
+const concreteMaterial = new CANNON.Material("concrete")
+const plasticMaterial = new CANNON.Material("plastic")
+
+const concretePlasticContactMaterial = new CANNON.ContactMaterial(
+   concreteMaterial,
+   plasticMaterial,
+   {
+      friction: 0.1,
+      restitution: 0.7
+   }
+)
+world.addContactMaterial(concretePlasticContactMaterial)
+
 const sphereShape = new CANNON.Sphere(0.5)
 const sphereBody = new CANNON.Body({
    mass: 1,
    position: new CANNON.Vec3(0, 3, 0),
-   shape: sphereShape
+   shape: sphereShape,
+   material: plasticMaterial
 })
 
 const floorShape = new CANNON.Plane()
@@ -49,6 +63,7 @@ const floorBody = new CANNON.Body()
 floorBody.mass = 0
 floorBody.addShape(floorShape)
 floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5)
+floorBody.material = concreteMaterial
 world.addBody(floorBody)
 
 world.addBody(sphereBody)
