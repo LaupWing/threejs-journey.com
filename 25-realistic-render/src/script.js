c5df6df3d0a23fe734e25c3647cf042b95e71783
LaupWing <loc_strict@hotmail.com>
2022-11-11 17:56:25 +0100

    episode 25

--- 25-realistic-render/src/script.js
+++ 25-realistic-render/src/script.js
@@ -2,49 +2,101 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
+
+
+const gltfLoader = new GLTFLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader()
 
 /**
  * Base
  */
 // Debug
 const gui = new dat.GUI()
-
+const debugObject = {}
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Test sphere
- */
-const testSphere = new THREE.Mesh(
-    new THREE.SphereGeometry(1, 32, 32),
-    new THREE.MeshBasicMaterial()
+const updateAllMaterials = () => {
+   scene.traverse((child) => {
+
+      if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
+
+         child.material.envMapIntensity = debugObject.envMapIntensity
+      }
+   })
+}
+
+// Envirionmentmap
+const environmentMap = cubeTextureLoader.load([
+   "/textures/environmentMaps/0/px.jpg",
+   "/textures/environmentMaps/0/nx.jpg",
+   "/textures/environmentMaps/0/py.jpg",
+   "/textures/environmentMaps/0/ny.jpg",
+   "/textures/environmentMaps/0/pz.jpg",
+   "/textures/environmentMaps/0/nz.jpg",
+])
+
+scene.background = environmentMap
+scene.environment = environmentMap
+
+debugObject.envMapIntensity = 5
+gui.add(debugObject, "envMapIntensity").min(0).max(10).step(0.001).onChange(() => {
+   updateAllMaterials()
+})
+
+// Models
+gltfLoader.load(
+   "/models/FlightHelmet/glTF/FlightHelmet.gltf",
+   (gltf) => {
+      gltf.scene.scale.set(10, 10, 10)
+      gltf.scene.position.set(0, -4, 0)
+      gltf.scene.rotation.y = Math.PI * 0.5
+      scene.add(gltf.scene)
+
+      gui.add(gltf.scene.rotation, "y").min(-Math.PI)
+         .max(Math.PI)
+         .step(0.001)
+         .name("rotation")
+
+      updateAllMaterials()
+   }
 )
-scene.add(testSphere)
+
+// Lights
+const directionalLight = new THREE.DirectionalLight("#ffffff", 3)
+directionalLight.position.set(0.25, 3, -2.25)
+scene.add(directionalLight)
+
+gui.add(directionalLight, "intensity").min(0).max(10).step(0.001).name("lightIntensity")
+gui.add(directionalLight.position, "x").min(-5).max(5).step(0.001).name("lightX")
+gui.add(directionalLight.position, "y").min(-5).max(5).step(0.001).name("lightY")
+gui.add(directionalLight.position, "z").min(-5).max(5).step(0.001).name("lightZ")
+
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+   width: window.innerWidth,
+   height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+   // Update sizes
+   sizes.width = window.innerWidth
+   sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+   // Update camera
+   camera.aspect = sizes.width / sizes.height
+   camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+   // Update renderer
+   renderer.setSize(sizes.width, sizes.height)
+   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -63,24 +115,25 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+   canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.physicallyCorrectLights = true
+renderer.outputEncoding = THREE.sRGBEncoding
 
 /**
  * Animate
  */
-const tick = () =>
-{
-    // Update controls
-    controls.update()
+const tick = () => {
+   // Update controls
+   controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+   // Render
+   renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+   // Call tick again on the next frame
+   window.requestAnimationFrame(tick)
 }
 
 tick()
