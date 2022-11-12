767e37b7726ea0480062ecf57b79425a457697f5
LaupWing <loc_strict@hotmail.com>
2022-11-12 05:58:36 +0100

    updateg

--- 25-realistic-render/src/script.js
+++ 25-realistic-render/src/script.js
@@ -53,9 +53,9 @@ gui.add(debugObject, "envMapIntensity").min(0).max(10).step(0.001).onChange(() =
 
 // Models
 gltfLoader.load(
-   "/models/FlightHelmet/glTF/FlightHelmet.gltf",
+   "/models/hamburger.glb",
    (gltf) => {
-      gltf.scene.scale.set(10, 10, 10)
+      gltf.scene.scale.set(0.3, 0.3, 0.3)
       gltf.scene.position.set(0, -4, 0)
       gltf.scene.rotation.y = Math.PI * 0.5
       scene.add(gltf.scene)
@@ -75,6 +75,7 @@ directionalLight.position.set(0.25, 3, -2.25)
 directionalLight.castShadow = true
 directionalLight.shadow.camera.far = 15
 directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.normalBias = 0.05
 scene.add(directionalLight)
 
 const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
