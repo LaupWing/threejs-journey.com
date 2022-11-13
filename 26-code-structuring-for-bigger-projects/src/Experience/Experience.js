c3d74c3560bdff91696920ca72b9638a2a67971e
LaupWing <loc_strict@hotmail.com>
2022-11-13 05:48:27 +0100

    resize

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -3,6 +3,7 @@ import Sizes from "./Utils/Sizes"
 export default class Experience {
    constructor(canvas) {
       window.experience = this
+
       this.canvas = canvas
       this.sizes = new Sizes()
    }
--- 26-code-structuring-for-bigger-projects/src/Experience/Utils/Sizes.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Utils/Sizes.js
@@ -3,5 +3,11 @@ export default class Sizes {
       this.width = window.innerWidth
       this.height = window.innerHeight
       this.pixelRatio = Math.min(window.devicePixelRatio, 2)
+
+      window.addEventListener("resize", () => {
+         this.width = window.innerWidth
+         this.height = window.innerHeight
+         this.pixelRatio = Math.min(window.devicePixelRatio, 2)
+      })
    }
 }
