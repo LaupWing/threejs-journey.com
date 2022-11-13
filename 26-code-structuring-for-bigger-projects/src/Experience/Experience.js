4500fb11cff3809d0b83bed2e3f229a156932ae8
LaupWing <loc_strict@hotmail.com>
2022-11-13 06:18:55 +0100

    update

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -1,3 +1,5 @@
+import * as THREE from "three"
+import Camera from "./Camera"
 import Sizes from "./Utils/Sizes"
 import Time from "./Utils/Time"
 
@@ -9,6 +11,8 @@ export default class Experience {
 
       this.sizes = new Sizes()
       this.time = new Time()
+      this.scene = new THREE.Scene()
+      this.camera = new Camera()
 
       this.sizes.on("resize", () => {
          this.resize()
