a11ff69dd0c351761851a5de8affd3e6c824f1c9
LaupWing <loc_strict@hotmail.com>
2022-11-13 06:30:59 +0100

    set orbit controls

--- 26-code-structuring-for-bigger-projects/src/Experience/Camera.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Camera.js
@@ -1,5 +1,6 @@
 import * as THREE from "three"
 import Experience from "./Experience"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 
 export default class Camera {
    constructor() {
@@ -8,5 +9,24 @@ export default class Camera {
       this.scene = this.experience.scene
       this.canvas = this.experience.canvas
 
+      this.setInstance()
+      this.setOrbitControls()
+   }
+
+   setInstance() {
+      this.instance = new THREE.PerspectiveCamera(
+         35,
+         this.sizes.width / this.sizes.height,
+         0.1,
+         100
+      )
+
+      this.instance.position.set(6, 4, 8)
+      this.scene.add(this.instance)
+   }
+
+   setOrbitControls() {
+      this.controls = new OrbitControls(this.instance, this.canvas)
+      this.controls.enableDamping = true
    }
 }
