8bb2aa840e0976aabaf5905d4cdcc786edbab560
LaupWing <loc_strict@hotmail.com>
2022-11-13 05:53:02 +0100

    event emiter

--- 26-code-structuring-for-bigger-projects/src/Experience/Utils/Sizes.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Utils/Sizes.js
@@ -1,3 +1,5 @@
+import EventEmitter from "./EventEmitter"
+
 export default class Sizes {
    constructor() {
       this.width = window.innerWidth
@@ -8,6 +10,7 @@ export default class Sizes {
          this.width = window.innerWidth
          this.height = window.innerHeight
          this.pixelRatio = Math.min(window.devicePixelRatio, 2)
+         EventEmitter
       })
    }
 }
