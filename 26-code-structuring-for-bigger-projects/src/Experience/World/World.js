e200ac64e85f32dd9606636e3c16d7b64d2dc935
LaupWing <loc_strict@hotmail.com>
2022-11-14 16:57:43 +0100

    update

--- 26-code-structuring-for-bigger-projects/src/Experience/World/World.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/World/World.js
@@ -1,6 +1,7 @@
 import Experience from "../Experience"
 import * as THREE from "three"
 import Environment from "./Environment"
+import Floor from "./Floor"
 
 export default class World {
    constructor() {
@@ -8,13 +9,8 @@ export default class World {
       this.scene = this.experience.scene
       this.resources = this.experience.resources
 
-      const testMesh = new THREE.Mesh(
-         new THREE.BoxGeometry(1, 1, 1),
-         new THREE.MeshStandardMaterial()
-      )
-      this.scene.add(testMesh)
-
       this.resources.on("ready", () => {
+         this.floor = new Floor()
          this.environment = new Environment()
       })
 
