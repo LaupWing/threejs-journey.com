ec6efef6cc9af760e1679b612716b8c98020e188
LaupWing <loc_strict@hotmail.com>
2022-11-14 05:50:53 +0100

    update

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -1,6 +1,7 @@
 import * as THREE from "three"
 import Camera from "./Camera"
 import Renderer from "./Renderer"
+import sources from "./sources"
 import Resources from "./Utils/Resources"
 import Sizes from "./Utils/Sizes"
 import Time from "./Utils/Time"
@@ -22,7 +23,7 @@ export default class Experience {
       this.sizes = new Sizes()
       this.time = new Time()
       this.scene = new THREE.Scene()
-      this.resources = new Resources()
+      this.resources = new Resources(sources)
       this.camera = new Camera()
       this.renderer = new Renderer()
       this.world = new World()
--- 26-code-structuring-for-bigger-projects/src/Experience/Utils/Resources.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Utils/Resources.js
@@ -1,7 +1,23 @@
 import EventEmitter from "./EventEmitter"
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import * as THREE from "three"
 
 export default class Resources extends EventEmitter {
-   constructor(canvas) {
+   constructor(sources) {
       super()
+      this.sources = sources
+
+      this.items = {}
+      this.toLoad = this.sources.length
+      this.loaded = 0
+
+      this.setLoaders()
+   }
+
+   setLoaders() {
+      this.loaders = {}
+      this.loaders.gltfLoader = new GLTFLoader()
+      this.loaders.textureLoader = new THREE.TextureLoader()
+      this.loaders.cubeTextureLoader = new THREE.CubeTextureLoader()
    }
 }
