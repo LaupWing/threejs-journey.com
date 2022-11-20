2d3cc8684750b440748b92014f94e3ce068fb9fe
LaupWing <loc_strict@hotmail.com>
2022-11-20 06:57:51 +0100

    update

--- 30-animated-galaxy/src/shaders/galaxy/fragment.glsl
+++ 30-animated-galaxy/src/shaders/galaxy/fragment.glsl
@@ -3,5 +3,13 @@ void main(){
    // strength = step(0.5, strength);
    // strength = 1.0 - strength;
 
-   gl_FragColor = vec4(vec3(strength), 1.0,);
+   // float strength = distance(gl_PointCoord, vec2(0.5));
+   // strength *= 0.2;
+   // strength = 1.0 -strength;
+
+   // float strength = distance(gl_PointCoord, vec2(0.5));
+   // strength *= 0.2;
+   // strength = 1.0 -strength;
+
+   gl_FragColor = vec4(vec3(strength), 1.0);
 }
