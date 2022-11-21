4d19c47c782c9eaeace59562b1f0c5abd6089bf7
LaupWing <loc_strict@hotmail.com>
2022-11-21 14:01:23 +0100

    update

--- 32-post-processing/src/script.js
+++ 32-post-processing/src/script.js
@@ -6,6 +6,10 @@ import * as dat from "lil-gui"
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js"
 import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js"
 import { DotScreenPass } from "three/examples/jsm/postprocessing/DotScreenPass.js"
+import { GlitchPass } from "three/examples/jsm/postprocessing/GlitchPass"
+import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass"
+import { RGBShiftShader } from "three/examples/jsm/shaders/RGBShiftShader"
+import { GammaCorrectionShader } from "three/examples/jsm/shaders/GammaCorrectionShader"
 
 /**
  * Base
@@ -101,6 +105,9 @@ window.addEventListener("resize", () => {
    // Update renderer
    renderer.setSize(sizes.width, sizes.height)
    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+   effectComposer.setSize(sizes.width, sizes.height)
+   effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -144,8 +151,19 @@ const renderPass = new RenderPass(scene, camera)
 effectComposer.addPass(renderPass)
 
 const dotScreenPass = new DotScreenPass()
+dotScreenPass.enabled = false
 effectComposer.addPass(dotScreenPass)
 
+const glitchPass = new GlitchPass()
+glitchPass.enabled = true
+effectComposer.addPass(glitchPass)
+
+const rgbShiftPass = new ShaderPass(RGBShiftShader)
+effectComposer.addPass(rgbShiftPass)
+rgbShiftPass.enabled = false
+
+const gammaCorrectionShader = new ShaderPass(GammaCorrectionShader)
+effectComposer.addPass(gammaCorrectionShader)
 /**
  * Animate
  */
