e9ec56926e956b6b6120dd672571c17bb5a36e06
LaupWing <loc_strict@hotmail.com>
2022-11-22 05:44:27 +0100

    doesnt work

--- 32-post-processing/src/script.js
+++ 32-post-processing/src/script.js
@@ -215,6 +215,8 @@ rgbShiftPass.enabled = false
 const DisplacementShader = {
    uniforms: {
       tDiffuse: { value: null },
+      uTime: { value: null },
+      uNormalMap: { value: null },
    },
    vertextShader: `
       varying vec2 vUv;
@@ -227,10 +229,16 @@ const DisplacementShader = {
    `,
    fragmentShader: `
       uniform sampler2D tDiffuse;
+      uniform float uTime;
 
       varying vec2 vUv;
 
       void main(){
+         vec2 newUv = vec2(
+            vUv.x,
+            vUv.y + sin(vUv.x * 10.0 + uTime) * 0.1
+         ); 
+
          vec4 color = texture2D(tDiffuse, vUv);
          gl_FragColor = color;
       }
@@ -238,6 +246,7 @@ const DisplacementShader = {
 }
 
 const displacementPass = new ShaderPass(DisplacementShader)
+displacementPass.material.uniforms.uTime.value = 0
 effectComposer.addPass(displacementPass)
 
 // gui.add(tintPass.material.uniforms.uTint.value, "x")
@@ -271,6 +280,8 @@ const clock = new THREE.Clock()
 
 const tick = () => {
    const elapsedTime = clock.getElapsedTime()
+
+   displacementPass.material.uniforms.uTime.value = elapsedTime
    // Update controls
    controls.update()
 
