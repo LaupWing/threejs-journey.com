b761fb178a3401f01cf219528057b74ed49498d5
LaupWing <loc_strict@hotmail.com>
2022-11-29 05:37:29 +0100

    upload

--- 38-importing-and-optimizing-the-scene/src/script.js
+++ 38-importing-and-optimizing-the-scene/src/script.js
@@ -1,20 +1,20 @@
-import './style.css'
-import * as dat from 'lil-gui'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import "./style.css"
+import * as dat from "lil-gui"
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js"
 
 /**
  * Base
  */
 // Debug
 const gui = new dat.GUI({
-    width: 400
+   width: 400,
 })
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl")
 
 // Scene
 const scene = new THREE.Scene()
@@ -27,7 +27,7 @@ const textureLoader = new THREE.TextureLoader()
 
 // Draco loader
 const dracoLoader = new DRACOLoader()
-dracoLoader.setDecoderPath('draco/')
+dracoLoader.setDecoderPath("draco/")
 
 // GLTF loader
 const gltfLoader = new GLTFLoader()
@@ -37,40 +37,63 @@ gltfLoader.setDRACOLoader(dracoLoader)
  * Object
  */
 const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
+   new THREE.BoxGeometry(1, 1, 1),
+   new THREE.MeshBasicMaterial()
 )
 
 scene.add(cube)
+
+// Model
+
+const bakedMaterial = new THREE.MeshBasicMaterial({
+   color: 0xff0000
+})
+gltfLoader.load(
+   "portal2.glb", 
+   (gltf) => {
+      console.log(gltf)
+      gltf.scene.traverse((child)=>{
+         console.log(child)
+         child.material = bakedMaterial
+      })
+      // console.log(gltf)
+      scene.add(gltf.scene)
+})
+
+
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+   width: window.innerWidth,
+   height: window.innerHeight,
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener("resize", () => {
+   // Update sizes
+   sizes.width = window.innerWidth
+   sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+   // Update camera
+   camera.aspect = sizes.width / sizes.height
+   camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+   // Update renderer
+   renderer.setSize(sizes.width, sizes.height)
+   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(
+   45,
+   sizes.width / sizes.height,
+   0.1,
+   100
+)
 camera.position.x = 4
 camera.position.y = 2
 camera.position.z = 4
@@ -84,8 +107,8 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true
+   canvas: canvas,
+   antialias: true,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -95,18 +118,17 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+   const elapsedTime = clock.getElapsedTime()
 
-    // Update controls
-    controls.update()
+   // Update controls
+   controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+   // Render
+   renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+   // Call tick again on the next frame
+   window.requestAnimationFrame(tick)
 }
 
 tick()
