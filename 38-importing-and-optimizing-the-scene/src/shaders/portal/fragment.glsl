1be0ee5c3d71f739a47bae48d851ec852d0dbd61
LaupWing <loc_strict@hotmail.com>
2022-11-30 13:38:11 +0100

    step

--- 38-importing-and-optimizing-the-scene/src/shaders/portal/fragment.glsl
+++ 38-importing-and-optimizing-the-scene/src/shaders/portal/fragment.glsl
@@ -82,7 +82,8 @@ void main(){
 
    float strength = cnoise(vec3(displacedUv * 5.0, uTime * 0.2));
 
-   float outerGlow = distance(vUv, vec2(0.5));
+   float outerGlow = distance(vUv, vec2(0.5)) * 5.0 - 1.4;
+   strength += outerGlow;
 
    gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
