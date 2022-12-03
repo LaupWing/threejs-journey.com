a4f479fd23b404ffdd6289b1a8162afc9845ae25
LaupWing <loc_strict@hotmail.com>
2022-12-03 17:16:56 +0100

    update

--- 42-first-r3f-application/src/Experience.js
+++ 42-first-r3f-application/src/Experience.js
@@ -3,9 +3,21 @@ import React from "react"
 const Experience = () => {
    return (
       <>
-         <mesh>
-            <torusKnotGeometry />
-            <meshNormalMaterial />
+         <mesh rotateY={Math.PI * 0.5} position-x={1} scale={1.5}>
+            {/* <sphereGeometry args={[1.5, 32, 32]} /> */}
+            <boxGeometry/>
+            <meshBasicMaterial 
+               color={'red'}
+               wireframe
+            />
+         </mesh>
+         <mesh rotateY={Math.PI * 0.5}  scale={1.5}>
+            {/* <sphereGeometry args={[1.5, 32, 32]} /> */}
+            <boxGeometry/>
+            <meshBasicMaterial 
+               color={'red'}
+               wireframe
+            />
          </mesh>
       </>
    )
