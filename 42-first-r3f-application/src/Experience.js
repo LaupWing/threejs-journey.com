6068f4f22e14ebf44347c4a9d228e21ef2294ef8
LaupWing <loc_strict@hotmail.com>
2022-12-03 16:59:50 +0100

    update

---
+++ 42-first-r3f-application/src/Experience.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Experience = () => {
+   return (
+      <>
+         <mesh>
+            <torusKnotGeometry />
+            <meshNormalMaterial />
+         </mesh>
+      </>
+   )
+}
+
+export default Experience
