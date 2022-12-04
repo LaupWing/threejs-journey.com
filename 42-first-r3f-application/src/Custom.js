868bf9e1c65481c36a840fd523bca3e136b880c8
LaupWing <loc_strict@hotmail.com>
2022-12-04 06:09:53 +0100

    update

--- 42-first-r3f-application/src/Custom.js
+++ 42-first-r3f-application/src/Custom.js
@@ -1,15 +1,23 @@
 import React from "react"
+import * as THREE from "three"
 
 const Custom = () => {
    const verticesCount = 10 * 3
    const positions = new Float32Array(verticesCount * 3)
-   for(let i = 0; i < verticesCount * 3; i++)
+   for (let i = 0; i < verticesCount * 3; i++)
       positions[i] = (Math.random() - 0.5) * 3
 
    return (
       <mesh>
-         <boxGeometry/>
-         <meshBasicMaterial color={"red"}/>
+         <bufferGeometry>
+            <bufferAttribute
+               attach={"attributes-position"}
+               count={verticesCount}
+               itemSize={3}
+               array={positions}
+            />
+         </bufferGeometry>
+         <meshBasicMaterial color={"red"} side={ THREE.DoubleSide }/>
       </mesh>
    )
 }
