99e90a8abcce767382f867c6f8fdb5cdde466544
LaupWing <loc_strict@hotmail.com>
2022-12-05 14:27:41 +0100

    object ref

--- 43-r3f-drei/src/Experience.js
+++ 43-r3f-drei/src/Experience.js
@@ -1,23 +1,28 @@
 import { OrbitControls, TransformControls } from "@react-three/drei"
+import { useRef } from "react"
 
 export default function Experience() {
-   
+   const cube = useRef()
+
    return (
       <>
          <OrbitControls />
          <directionalLight position={[1, 2, 3]} intensity={1.5} />
          <ambientLight intensity={0.5} />
-         <TransformControls>
             <mesh position-x={-2}>
                <sphereGeometry />
                <meshStandardMaterial color="orange" />
             </mesh>
-         </TransformControls>
 
-         <mesh position-x={2} scale={1.5}>
+         <mesh 
+            ref={cube} 
+            scale={1.5} 
+            position-x={2}
+         >
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
          </mesh>
+         <TransformControls object={cube}/>
 
          <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
             <planeGeometry />
