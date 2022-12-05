412be96bfa150fb47c35d8f6b188c348c9ed7755
LaupWing <loc_strict@hotmail.com>
2022-12-05 14:32:35 +0100

    trying out pivot

--- 43-r3f-drei/src/Experience.js
+++ 43-r3f-drei/src/Experience.js
@@ -1,4 +1,4 @@
-import { OrbitControls, TransformControls } from "@react-three/drei"
+import { OrbitControls, PivotControls, TransformControls } from "@react-three/drei"
 import { useRef } from "react"
 
 export default function Experience() {
@@ -6,7 +6,7 @@ export default function Experience() {
 
    return (
       <>
-         <OrbitControls />
+         <OrbitControls makeDefault/>
          <directionalLight position={[1, 2, 3]} intensity={1.5} />
          <ambientLight intensity={0.5} />
             <mesh position-x={-2}>
@@ -22,7 +22,7 @@ export default function Experience() {
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
          </mesh>
-         <TransformControls object={cube}/>
+         <PivotControls object={cube}/>
 
          <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
             <planeGeometry />
