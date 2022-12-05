23fea1c8177e921c115b5b646b24396007641f33
LaupWing <loc_strict@hotmail.com>
2022-12-05 05:59:46 +0100

    update

--- 43-r3f-drei/src/Experience.js
+++ 43-r3f-drei/src/Experience.js
@@ -1,33 +1,26 @@
-import { useThree, extend } from '@react-three/fiber'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-extend({ OrbitControls })
+export default function Experience() {
 
-export default function Experience()
-{
-    const { camera, gl } = useThree()
+   return (
+      <>
 
-    return <>
+         <directionalLight position={[1, 2, 3]} intensity={1.5} />
+         <ambientLight intensity={0.5} />
 
-        <orbitControls args={ [ camera, gl.domElement ] } />
-
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
-
-        <mesh position-x={ - 2 }>
+         <mesh position-x={-2}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
-        </mesh>
+         </mesh>
 
-        <mesh position-x={ 2 } scale={ 1.5 }>
+         <mesh position-x={2} scale={1.5}>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+         </mesh>
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+         <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
-    </>
+         </mesh>
+      </>
+   )
 }
