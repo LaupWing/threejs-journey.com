953666e7306c30ddc3fb6a300e50e927778b0f0e
LaupWing <loc_strict@hotmail.com>
2022-12-05 14:41:27 +0100

    update

--- 43-r3f-drei/src/Experience.js
+++ 43-r3f-drei/src/Experience.js
@@ -9,10 +9,20 @@ export default function Experience() {
          <OrbitControls makeDefault/>
          <directionalLight position={[1, 2, 3]} intensity={1.5} />
          <ambientLight intensity={0.5} />
+
+         <PivotControls 
+            depthTest={false} 
+            anchor={[0, 0, 0]}
+            lineWidth={4}
+            axisColors={["#9381ff", "#ff4d6d", "#7ae582"]}
+            scale={100}
+            fixed={true}
+         >
             <mesh position-x={-2}>
                <sphereGeometry />
                <meshStandardMaterial color="orange" />
             </mesh>
+         </PivotControls>
 
          <mesh 
             ref={cube} 
@@ -22,7 +32,7 @@ export default function Experience() {
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
          </mesh>
-         <PivotControls object={cube}/>
+         <TransformControls object={cube}/>
 
          <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
             <planeGeometry />
