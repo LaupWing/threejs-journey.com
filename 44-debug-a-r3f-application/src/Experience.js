b97f8e99d29dadb7452cc74d7230d811c63eb6d7
LaupWing <loc_strict@hotmail.com>
2022-12-06 05:32:49 +0100

    sperartted

--- 44-debug-a-r3f-application/src/Experience.js
+++ 44-debug-a-r3f-application/src/Experience.js
@@ -1,28 +1,25 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls } from "@react-three/drei"
+import Cube from "./Cube"
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+   return (
+      <>
+         <OrbitControls makeDefault />
 
-        <OrbitControls makeDefault />
+         <directionalLight position={[1, 2, 3]} intensity={1.5} />
+         <ambientLight intensity={0.5} />
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
-
-        <mesh position-x={ - 2 }>
+         <mesh position-x={-2}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
-        </mesh>
+         </mesh>
 
-        <mesh position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+         <Cube />
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+         <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
-    </>
+         </mesh>
+      </>
+   )
 }
