39b7d9d6e011266c4e897f7b64650ba77ad4640f
LaupWing <loc_strict@hotmail.com>
2022-12-08 05:22:58 +0100

    update

--- 46-load-models-with-r3f/src/Experience.js
+++ 46-load-models-with-r3f/src/Experience.js
@@ -1,31 +1,35 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
+import { OrbitControls } from "@react-three/drei"
+import { useLoader } from "@react-three/fiber"
+import { Perf } from "r3f-perf"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+   const model = useLoader(GLTFLoader, "./hamburger.glb")
 
-        <Perf position="top-left" />
 
-        <OrbitControls makeDefault />
+   return (
+      <>
+         <Perf position="top-left" />
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+         <OrbitControls makeDefault />
 
-        <mesh castShadow position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+         <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+         <ambientLight intensity={0.5} />
 
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-
-        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+         <mesh
+            receiveShadow
+            position-y={-1}
+            rotation-x={-Math.PI * 0.5}
+            scale={10}
+         >
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
-        </mesh>
+         </mesh>
 
-    </>
+         <primitive
+            object={model.scene}
+            scale={0.35}
+         />
+      </>
+   )
 }
