202a97a53fa0ae3cd8cc3dff9e6a9d96c3f107d1
LaupWing <loc_strict@hotmail.com>
2022-12-08 17:01:42 +0100

    update

--- 46-load-models-with-r3f/src/Fox.js
+++ 46-load-models-with-r3f/src/Fox.js
@@ -1,14 +1,20 @@
-import { useGLTF } from "@react-three/drei"
+import { useAnimations, useGLTF } from "@react-three/drei"
 import React from "react"
+import { useEffect } from "react"
 
 const Fox = () => {
    const fox = useGLTF("./Fox/glTF/Fox.gltf")
-
+   const animations = useAnimations(fox.animations, fox.scene)
+   useEffect(() => {
+      const action = animations.actions.Run
+      action.play()
+   }, [])
    return (
       <primitive 
          object={fox.scene}
-         position={[-0.25, 0, 2.5]}
+         position={[-2.5, 0, 2.5]}
          rotation-y={0.3}
+         scale={0.02}
       />
    )
 }
