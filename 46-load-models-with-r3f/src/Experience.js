41549e2ba50109525c5d7dad23591468a49fabbb
LaupWing <loc_strict@hotmail.com>
2022-12-08 05:44:08 +0100

    fallback value

--- 46-load-models-with-r3f/src/Experience.js
+++ 46-load-models-with-r3f/src/Experience.js
@@ -1,18 +1,20 @@
 import { OrbitControls } from "@react-three/drei"
 import { Perf } from "r3f-perf"
+import { Suspense } from "react"
 import Model from "./Model"
 
 export default function Experience() {
-
-
-
    return (
       <>
          <Perf position="top-left" />
 
          <OrbitControls makeDefault />
 
-         <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+         <directionalLight 
+            castShadow 
+            position={[1, 2, 3]} 
+            intensity={1.5} 
+         />
          <ambientLight intensity={0.5} />
 
          <mesh
@@ -24,7 +26,16 @@ export default function Experience() {
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
          </mesh>
-         <Model/>
+         <Suspense
+            fallback={
+               <mesh position-y={0.5} scale={[2, 3, 2]}>
+                  <boxGeometry args={[1, 1, 1, 2, 2, 2]} />
+                  <meshBasicMaterial wireframe color="red" />
+               </mesh>
+            }
+         >
+            <Model />
+         </Suspense>
       </>
    )
 }
