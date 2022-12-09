2a8bc7a93456c50e68c23779e4f4b67bf4097366
LaupWing <loc_strict@hotmail.com>
2022-12-09 05:17:43 +0100

    udpate

--- 46-load-models-with-r3f/src/Fox.js
+++ 46-load-models-with-r3f/src/Fox.js
@@ -14,9 +14,14 @@ const Fox = () => {
    })
 
    useEffect(() => {
-      animations.actions[animationName].play()
+      const action = animations.actions[animationName]
+      action.reset().fadeIn(0.5).play()
+
+      return () =>{
+         action.fadeOut(0.5)
+      }
    }, [animationName])
-   
+
    return (
       <primitive 
          object={fox.scene}
