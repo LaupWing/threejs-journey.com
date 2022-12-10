d42281f0083ceeb5e241fc0880f1af64c7066842
LaupWing <loc_strict@hotmail.com>
2022-12-10 13:42:17 +0100

    occlude

--- 49-mouse-events-with-r3f/src/Experience.js
+++ 49-mouse-events-with-r3f/src/Experience.js
@@ -1,37 +1,44 @@
-import { useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import { useRef } from 'react'
+import { useFrame } from "@react-three/fiber"
+import { OrbitControls } from "@react-three/drei"
+import { useRef } from "react"
 
-export default function Experience()
-{
-    const cube = useRef()
-    
-    useFrame((state, delta) =>
-    {
-        cube.current.rotation.y += delta * 0.2
-    })
+export default function Experience() {
+   const cube = useRef()
 
-    return <>
+   useFrame((state, delta) => {
+      cube.current.rotation.y += delta * 0.2
+   })
 
-        <OrbitControls makeDefault />
+   const eventHandler = () => { 
+      cube.current.material.color.set(`hsl${Math.random() * 360}, 100%, 75%`)
+   }
+ 
+   return (
+      <>
+         <OrbitControls makeDefault />
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+         <directionalLight position={[1, 2, 3]} intensity={1.5} />
+         <ambientLight intensity={0.5} />
 
-        <mesh position-x={ - 2 }>
+         <mesh position-x={-2}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
-        </mesh>
+         </mesh>
 
-        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
+         <mesh 
+            ref={cube} 
+            position-x={2} 
+            scale={1.5}
+            onClick={eventHandler}
+         >
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+         </mesh>
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+         <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
-    </>
+         </mesh>
+      </>
+   )
 }
