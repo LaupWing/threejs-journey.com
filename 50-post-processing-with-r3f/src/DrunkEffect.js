cce38425eda5b48bf78a2d04e29ecbb150703e8d
LaupWing <loc_strict@hotmail.com>
2022-12-12 14:23:43 +0100

    50

--- 50-post-processing-with-r3f/src/DrunkEffect.js
+++ 50-post-processing-with-r3f/src/DrunkEffect.js
@@ -1,12 +1,13 @@
-import { Effect } from "postprocessing"
+import { BlendFunction, Effect } from "postprocessing"
 import { Uniform } from "three"
 
 const fragmentShader = /* glsl */ `
    uniform float frequency;
    uniform float amplitude;
+   uniform float offset;
 
    void mainUv(inout vec2 uv){
-      uv.y += sin(uv.x * frequency) * amplitude;
+      uv.y += sin(uv.x * frequency * offset) * amplitude;
    }
 
    void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor){
@@ -15,16 +16,22 @@ const fragmentShader = /* glsl */ `
 `
 
 export default class DrunkEffect extends Effect {
-   constructor({ frequency, amplitude}) {
+   constructor({ frequency, amplitude, blendFunction = BlendFunction.DARKEN}) {
       super(
          "DrunkEffect", 
          fragmentShader, 
-         {
+         {  
+            blendFunction,
             uniforms: new Map([
                [ "frequency", new Uniform(frequency) ],
                [ "amplitude", new Uniform(amplitude) ],
+               [ "offset", new Uniform(0) ],
             ])
          }
       )
+   }
+
+   update(renderer, inputBuffer, deltaTime){
+      this.uniforms.get("offset").value += deltaTime  
    }
 }
--- 50-post-processing-with-r3f/src/Experience.js
+++ 50-post-processing-with-r3f/src/Experience.js
@@ -92,7 +92,7 @@ export default function Experience() {
             <Drunk 
                ref={drunkRef}
                {...drunkProps}
-               
+               blendFunction={BlendFunction.DARKEN}
             />
          </EffectComposer>
          <Perf position="top-left" />
