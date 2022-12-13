d12efdff8f5630009e637568d67a59e6ba3cc4ff
LaupWing <loc_strict@hotmail.com>
2022-12-13 06:00:03 +0100

    udpate

--- 52-physics-with-r3f/src/Experience.js
+++ 52-physics-with-r3f/src/Experience.js
@@ -15,12 +15,21 @@ export default function Experience() {
          <Physics>
             <Debug/>
             <RigidBody colliders="ball">
-               <mesh castShadow position={[-2, 2, 0]}>
+               <mesh castShadow position={[0, 4, 0]}>
                   <sphereGeometry />
                   <meshStandardMaterial color="orange" />
                </mesh>
             </RigidBody>
-            
+            <RigidBody colliders="hull">
+               <mesh 
+                  castShadow 
+                  position={[0, 1, 0]} 
+                  rotation={[Math.PI * 0.1, 0, 0]}
+               >
+                  <torusGeometry args={[1, 0.5, 16, 32]} />
+                  <meshStandardMaterial color={"mediumpurple"}/>
+               </mesh>
+            </RigidBody>
             <RigidBody type="fixed">
                <mesh receiveShadow position-y={-1.25}>
                   <boxGeometry args={[10, 0.5, 10]} />
