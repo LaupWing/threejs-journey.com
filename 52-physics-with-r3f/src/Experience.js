86920e4a9988c035455eecb908b45b698e9db3e8
LaupWing <loc_strict@hotmail.com>
2022-12-14 14:34:43 +0100

    update

--- 52-physics-with-r3f/src/Experience.js
+++ 52-physics-with-r3f/src/Experience.js
@@ -10,6 +10,7 @@ import {
 } from "@react-three/rapier"
 import { Perf } from "r3f-perf"
 import { useState } from "react"
+import { useMemo } from "react"
 import { useEffect } from "react"
 import { useRef } from "react"
 import * as THREE from "three"
@@ -23,17 +24,44 @@ export default function Experience() {
    const cubesCount = 3
    const cubes = useRef() 
 
-   useEffect(()=>{
+   const cubeTransforms = useMemo(()=>{
+      const positions = []
+      const rotations= []
+      const scales = []
+
       for(let i = 0; i < cubesCount; i++){
-         const matrix = new THREE.Matrix4()
-         matrix.compose(
-            new THREE.Vector3(i * 2, 0, 0),
-            new THREE.Quaternion(),
-            new THREE.Vector3(1, 1, 1),
-         )
-         cubes.current.setMatrix(i, matrix)
+         positions.push([
+            (Math.random() -0.5) * 8, 
+            6 + 1 * 0.2, 
+            (Math.random() -0.5) * 8, 
+         ])
+         rotations.push([
+            Math.random(), 
+            Math.random(), 
+            Math.random(), 
+         ])
+         const scale = 0.2 + Math.random() * 0.8
+         scales.push([scale, scale, scale])
       }
-   },[])
+
+      return {
+         positions,
+         rotations,
+         scales
+      }
+   }, [])
+
+   // useEffect(()=>{
+   //    for(let i = 0; i < cubesCount; i++){
+   //       const matrix = new THREE.Matrix4()
+   //       matrix.compose(
+   //          new THREE.Vector3(i * 2, 0, 0),
+   //          new THREE.Quaternion(),
+   //          new THREE.Vector3(1, 1, 1),
+   //       )
+   //       cubes.current.setMatrix(i, matrix)
+   //    }
+   // },[])
 
    useFrame((state) => {
       const time = state.clock.getElapsedTime()
@@ -132,7 +160,11 @@ export default function Experience() {
                <CuboidCollider args={[0.5, 2, 5]} position={[5.5, 1, 0]} />
                <CuboidCollider args={[0.5, 2, 5]} position={[-5.5, 1, 0]} />
             </RigidBody>
-            <InstancedRigidBodies>
+            <InstancedRigidBodies 
+               positions={cubeTransforms.positions}
+               rotations={cubeTransforms.rotations}
+               scales={cubeTransforms.scales}
+            >
                <instancedMesh ref={cubes} castShadow receiveShadow args={[null, null, cubesCount]}>
                   <boxGeometry />
                   <meshStandardMaterial color={"tomato"}/>
