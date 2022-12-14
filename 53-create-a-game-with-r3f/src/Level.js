69265d751d1b83d560564523e8db21b043a14350
LaupWing <loc_strict@hotmail.com>
2022-12-14 15:16:08 +0100

    udpate

--- 53-create-a-game-with-r3f/src/Level.js
+++ 53-create-a-game-with-r3f/src/Level.js
@@ -1,4 +1,7 @@
+import { RigidBody } from "@react-three/rapier"
 import * as THREE from "three"
+
+THREE.ColorManagement.legacyMode = false
 
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
 
@@ -15,12 +18,36 @@ function BlockStart({
       <group position={position}>
          <mesh 
             position={[0, -0.1, 0]}
+            material={floor1Material}
             receiveShadow
             geometry={boxGeometry}
             scale={[4, 0.2, 4]}
-         >
-            <meshStandardMaterial color={"limegreen"}/>
-         </mesh>
+         />
+      </group>
+   )
+}
+
+function BlockSpinner({
+   position = [0, 0, 0]
+}) {
+   return (
+      <group position={position}>
+         <mesh 
+            geometry={boxGeometry}
+            position={[0, -0.1, 0]}
+            material={floor2Material}
+            scale={[4, 0.2, 4]}
+            receiveShadow
+         />
+         <RigidBody>
+            <mesh 
+               geometry={boxGeometry}
+               material={obstacleMaterial}
+               scale={[3.5, 0.3, 0.3]}
+               castShadow
+               receiveShadow
+            />
+         </RigidBody>
       </group>
    )
 }
@@ -28,7 +55,8 @@ function BlockStart({
 export default function Level() {
    return (
       <>
-         <BlockStart position={[0, 0, 0]}/>
+         <BlockStart position={[0, 0, 4]}/>
+         <BlockSpinner position={[0, 0, 0]}/>
       </>
    )
 }
