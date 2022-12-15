29d845abf7df6a3be3a77ebbae50613a30e43ae3
LaupWing <loc_strict@hotmail.com>
2022-12-15 05:22:16 +0100

    update

--- 53-create-a-game-with-r3f/src/Level.js
+++ 53-create-a-game-with-r3f/src/Level.js
@@ -1,4 +1,6 @@
+import { useFrame } from "@react-three/fiber"
 import { RigidBody } from "@react-three/rapier"
+import { useRef, useState } from "react"
 import * as THREE from "three"
 
 THREE.ColorManagement.legacyMode = false
@@ -30,6 +32,17 @@ function BlockStart({
 function BlockSpinner({
    position = [0, 0, 0]
 }) {
+   const obstacle = useRef()
+   const [speed] = useState(()=> (Math.random() + 0.2) * (Math.random() < 0.5 ? -1 : 1))
+
+   useFrame((state)=>{
+      const time = state.clock.getElapsedTime()
+
+      const rotation = new THREE.Quaternion()
+      rotation.setFromEuler(new THREE.Euler(0, time * speed, 0))
+      obstacle.current.setNextKinematicRotation(rotation)
+   })
+
    return (
       <group position={position}>
          <mesh 
@@ -39,7 +52,13 @@ function BlockSpinner({
             scale={[4, 0.2, 4]}
             receiveShadow
          />
-         <RigidBody>
+         <RigidBody 
+            type="kinematicPosition" 
+            position={[0, 0.3, 0]}
+            restitution={0.2}
+            friction={0}
+            ref={obstacle}
+         >
             <mesh 
                geometry={boxGeometry}
                material={obstacleMaterial}
