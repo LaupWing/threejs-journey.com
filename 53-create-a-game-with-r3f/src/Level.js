6739f659066b213e1e02344b84609622005413c9
LaupWing <loc_strict@hotmail.com>
2022-12-15 05:25:08 +0100

    Block spinner

--- 53-create-a-game-with-r3f/src/Level.js
+++ 53-create-a-game-with-r3f/src/Level.js
@@ -29,7 +29,7 @@ function BlockStart({
    )
 }
 
-function BlockSpinner({
+function BlockLimbo({
    position = [0, 0, 0]
 }) {
    const obstacle = useRef()
@@ -71,11 +71,51 @@ function BlockSpinner({
    )
 }
 
+function BlockSpinner({
+   position = [0, 0, 0]
+}) {
+   const obstacle = useRef()
+   const [speed] = useState(()=> (Math.random() + 0.2) * (Math.random() < 0.5 ? -1 : 1))
+
+   useFrame((state)=>{
+      const time = state.clock.getElapsedTime()
+
+   })
+
+   return (
+      <group position={position}>
+         <mesh 
+            geometry={boxGeometry}
+            position={[0, -0.1, 0]}
+            material={floor2Material}
+            scale={[4, 0.2, 4]}
+            receiveShadow
+         />
+         <RigidBody 
+            type="kinematicPosition" 
+            position={[0, 0.3, 0]}
+            restitution={0.2}
+            friction={0}
+            ref={obstacle}
+         >
+            <mesh 
+               geometry={boxGeometry}
+               material={obstacleMaterial}
+               scale={[3.5, 0.3, 0.3]}
+               castShadow
+               receiveShadow
+            />
+         </RigidBody>
+      </group>
+   )
+}
+
 export default function Level() {
    return (
       <>
-         <BlockStart position={[0, 0, 4]}/>
-         <BlockSpinner position={[0, 0, 0]}/>
+         <BlockStart position={[0, 0, 8]}/>
+         <BlockSpinner position={[0, 0, 4]}/>
+         <BlockLimbo position={[0, 0, 0]}/>
       </>
    )
 }
