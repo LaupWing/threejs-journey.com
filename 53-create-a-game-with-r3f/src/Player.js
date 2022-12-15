3cdde1d799c74afe493dba6e71b103ab5ea0cb59
LaupWing <loc_strict@hotmail.com>
2022-12-15 15:13:09 +0100

    udpate

--- 53-create-a-game-with-r3f/src/Player.js
+++ 53-create-a-game-with-r3f/src/Player.js
@@ -1,7 +1,16 @@
+import { useKeyboardControls } from "@react-three/drei"
+import { useFrame } from "@react-three/fiber"
 import { RigidBody } from "@react-three/rapier"
-import React from "react"
+import React, { useRef } from "react"
 
 const Player = () => {
+   const [subscribeKeys, getKeys] = useKeyboardControls()
+   const body = useRef()
+
+   useFrame(()=>{
+      const {forward, backward, leftward, rightward} = getKeys()
+   })
+
    return (
       <>
          <RigidBody 
