530847010d2d7e3b642a2cf79d268ed331b41ff4
LaupWing <loc_strict@hotmail.com>
2022-12-15 05:31:39 +0100

    Limbo finished

--- 53-create-a-game-with-r3f/src/Level.js
+++ 53-create-a-game-with-r3f/src/Level.js
@@ -71,7 +71,7 @@ function BlockLimbo({
    )
 }
 
-function BlockSpinner({
+function BlockAxe({
    position = [0, 0, 0]
 }) {
    const obstacle = useRef()
@@ -80,6 +80,55 @@ function BlockSpinner({
    useFrame((state)=>{
       const time = state.clock.getElapsedTime()
 
+      const rotation = new THREE.Quaternion()
+      rotation.setFromEuler(new THREE.Euler(0, time * speed, 0))
+      obstacle.current.setNextKinematicRotation(rotation)
+   })
+
+   return (
+      <group position={position}>
+         <mesh 
+            geometry={boxGeometry}
+            position={[0, -0.1, 0]}
+            material={floor2Material}
+            scale={[4, 0.2, 4]}
+            receiveShadow
+         />
+         <RigidBody 
+            type="kinematicPosition" 
+            position={[0, 0.3, 0]}
+            restitution={0.2}
+            friction={0}
+            ref={obstacle}
+         >
+            <mesh 
+               geometry={boxGeometry}
+               material={obstacleMaterial}
+               scale={[3.5, 0.3, 0.3]}
+               castShadow
+               receiveShadow
+            />
+         </RigidBody>
+      </group>
+   )
+}
+
+function BlockSpinner({
+   position = [0, 0, 0]
+}) {
+   const obstacle = useRef()
+   const [timeOffset] = useState(()=> Math.random() * Math.PI * 2)
+
+   useFrame((state)=>{
+      const time = state.clock.getElapsedTime()
+
+      const y = Math.sin(time * timeOffset) + 1.15
+
+      obstacle.current.setNextKinematicTranslation({
+         x: position[0],
+         y: y + position[1],
+         z: position[2]
+      })
    })
 
    return (
