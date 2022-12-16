f7f3f945c2161173a5758c5918713e27bbbeddbc
LaupWing <loc_strict@hotmail.com>
2022-12-16 15:06:49 +0100

    added

--- 53-create-a-game-with-r3f/src/Interface.js
+++ 53-create-a-game-with-r3f/src/Interface.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react"
 
 const Interface = () => {
    return (
       <div className="interface">
          <div className="time">0.00</div>
+         <div className="restart">Restart</div>
+         <div className="controls">
+            <div className="raw">
+               <div className="key"></div>
+            </div>
+            <div className="raw">
+               <div className="key"></div>
+               <div className="key"></div>
+               <div className="key"></div>
+            </div>
+            <div className="raw">
+               <div className="key large"></div>
+            </div>
+         </div>
       </div>
    )
 }
