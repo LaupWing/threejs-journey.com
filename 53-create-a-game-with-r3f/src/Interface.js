b865e29f06d111c87a39facf1bf48ad682430118
LaupWing <loc_strict@hotmail.com>
2022-12-16 15:01:22 +0100

    udpate

--- 53-create-a-game-with-r3f/src/Interface.js
+++ 53-create-a-game-with-r3f/src/Interface.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Interface = () => {
    return (
-      <div className="interface"></div>
+      <div className="interface">
+         <div className="time">0.00</div>
+      </div>
    )
 }
 
