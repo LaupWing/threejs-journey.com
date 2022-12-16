372457cb0b69b5e6927c084a130521da036d0c12
LaupWing <loc_strict@hotmail.com>
2022-12-16 05:51:31 +0100

    prevent double jump

--- 53-create-a-game-with-r3f/src/Player.js
+++ 53-create-a-game-with-r3f/src/Player.js
@@ -1,19 +1,34 @@
 import { useKeyboardControls } from "@react-three/drei"
 import { useFrame } from "@react-three/fiber"
-import { RigidBody } from "@react-three/rapier"
+import { RigidBody, useRapier } from "@react-three/rapier"
 import React, { useRef } from "react"
 import { useEffect } from "react"
 
 const Player = () => {
    const [subscribeKeys, getKeys] = useKeyboardControls()
    const body = useRef()
+   const {rapier, world} = useRapier()
 
    const jump = () => {
-      body.current.applyImpulse({
+      const origin = body.current.translation()
+      origin.y -= 0.31
+      const direction = {
          x: 0,
-         y: 0.5,
-         z: 0
-      })
+         y: -1,
+         z: 0 
+      }
+      const ray = new rapier.Ray(origin, direction)
+      const rapierWorld = world.raw()
+      const hit = rapierWorld.castRay(ray, 10, true)
+
+      if(hit.tio < 0.15){
+         body.current.applyImpulse({
+            x: 0,
+            y: 0.5,
+            z: 0
+         })
+      }
+
    }
 
    useEffect(()=>{
