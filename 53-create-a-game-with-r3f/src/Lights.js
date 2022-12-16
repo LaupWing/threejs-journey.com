ed11f2a5325160d7880c2dcfc83a7a15323de940
LaupWing <loc_strict@hotmail.com>
2022-12-16 14:54:29 +0100

    update

--- 53-create-a-game-with-r3f/src/Lights.js
+++ 53-create-a-game-with-r3f/src/Lights.js
@@ -5,7 +5,9 @@ export default function Lights() {
    const light = useRef()
 
    useFrame((state)=>{
-      light.current.position.z = state.camera.position.z + 1
+      light.current.position.z = state.camera.position.z + 1 -4
+      light.current.target.position.z = state.camera.position.z -4
+      light.current.target.updateMatrixWorld()
    })
 
    return (
